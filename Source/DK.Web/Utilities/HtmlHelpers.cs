99ee274b110468140fc4dabcd2e94ffa7de822c8
huyenvv90 <huyenvv90@gmail.com>
2021-01-11 22:28:39 +0700

    Update dropdown

---
+++ Source/DK.Web/Utilities/HtmlHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace DK.Web
+{
+    public static class DKHelpers
+    {
+        public static MvcHtmlString BuildDrodownList(this HtmlHelper html, string name, IEnumerable<string> list, string selected, string optionText = "-:-", string className = "form-control form-control-sm")
+        {
+            var newList = new List<string> { optionText };
+            newList.AddRange(list);
+
+            var tagSelect = new TagBuilder("select");
+
+            tagSelect.AddCssClass(className);
+            tagSelect.Attributes.Add("name", name);
+            tagSelect.Attributes.Add("id", name);
+            foreach (var item in newList)
+            {
+                var tagOption = new TagBuilder("option");
+                tagOption.SetInnerText(item);
+                if (!string.IsNullOrEmpty(item) && !string.IsNullOrEmpty(selected) && item.ToLower().Equals(selected.ToLower()))
+                {
+                    tagOption.Attributes.Add("selected", "selected");
+                }
+                tagSelect.InnerHtml += tagOption.ToString();
+            }
+            return new MvcHtmlString(tagSelect.ToString());
+        }
+    }
+}
